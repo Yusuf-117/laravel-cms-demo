50c6d7666a7a912e91e83050b64467375f8733d4
Yusuf <Yusufibrahim117@hotmail.com>
2026-03-28 12:56:56 +0000

    modify inertia progess bar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,28 +1,34 @@
-import './bootstrap';
-import '../css/app.css';
+import "./bootstrap";
+import "../css/app.css";
 
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy";
+import { theme } from '@/Theme/theme'
 
-import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
-import { ZiggyVue } from '../../vendor/tightenco/ziggy'
-
-const savedTheme = localStorage.getItem('theme')
-if (savedTheme === 'dark') {
-  document.documentElement.classList.add('dark')
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "dark") {
+    document.documentElement.classList.add("dark");
 } else {
-  document.documentElement.classList.remove('dark')
+    document.documentElement.classList.remove("dark");
 }
 
 createInertiaApp({
     title: (title) => `CMS Demo ${title}`,
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-        return pages[`./Pages/${name}.vue`]
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
+        return pages[`./Pages/${name}.vue`];
     },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .mount(el)
+            .mount(el);
     },
-})
+    progress: {
+        delay: 250,
+        color: theme.colors.primary,
+        includeCSS: true,
+        showSpinner: false,
+    },
+});
