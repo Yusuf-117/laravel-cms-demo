4b490acc823e9c0d57199ecc5fcadba5c91c7478
Yusuf <Yusufibrahim117@hotmail.com>
2026-03-28 12:42:47 +0000

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,12 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password'
-        ]);
+        // Viewer
+        User::updateOrCreate(
+            ['email' => 'viewer@example.com'],
+            [
+                'name' => 'Demo Viewer',
+                'password' => Hash::make('password'),
+                'role' => 'viewer',
+            ]
+        );
     }
 }
