79f0945317d0fa46f734085d120c4a1dcc934904
Yusuf <Yusufibrahim117@hotmail.com>
2026-04-06 01:48:15 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
+use App\Models\Category;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -24,6 +26,28 @@ class DatabaseSeeder extends Seeder
                 'password' => Hash::make('password'),
                 'role' => 'viewer',
             ]
+        );
+
+        $author = User::first();
+
+        $intro = Category::updateOrCreate(
+            ['slug' => 'intro'],
+            [
+                'name' => 'Intro',
+                'parent_id' => null,
+                'sort_order' => 1,
+            ]
+        );
+
+        Article::updateOrCreate(
+            ['slug' => 'introduction'],
+            [
+                'category_id' => $intro->id,
+                'author_id' => $author?->id,
+                'title' => 'Introduction',
+                'excerpt' => 'A quick overview of this CMS, why it exists, and how to safely explore the admin.',
+                'content' => 'First'
+                ]
         );
     }
 }
